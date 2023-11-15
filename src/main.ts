1e807f28fd6c5368aefe535b9f2daf5203e301c5
blue <336767@niuitmo.ru>
2023-11-15 19:40:34 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // prisma
   const prismaService = app.get(PrismaService);
   app.enableShutdownHooks();
