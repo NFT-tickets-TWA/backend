50faeb8fdae76e0a26f17693c202cf49471b55ba
blue <336767@niuitmo.ru>
2023-12-10 22:50:52 +0300

    db

---
+++ prisma/migrations/20231210195030_init/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - Made the column `finished_at` on table `Event` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `locationID` on table `Event` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `started_at` on table `Event` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Event" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "urlCover" TEXT,
+    "description" TEXT,
+    "isSBT" BOOLEAN NOT NULL,
+    "creatorID" INTEGER NOT NULL,
+    "started_at" DATETIME NOT NULL,
+    "finished_at" DATETIME NOT NULL,
+    "locationID" INTEGER NOT NULL,
+    "nftIpfsUrl" TEXT NOT NULL,
+    "contractAddress" TEXT,
+    "registeredParticipants" INTEGER NOT NULL DEFAULT 0,
+    "countOfRewardTokens" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "typeId" INTEGER NOT NULL DEFAULT 1,
+    "statusId" INTEGER NOT NULL DEFAULT 1,
+    "approveLink" TEXT,
+    CONSTRAINT "Event_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "EventType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Event_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "EventStatus" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Event_locationID_fkey" FOREIGN KEY ("locationID") REFERENCES "Location" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Event_creatorID_fkey" FOREIGN KEY ("creatorID") REFERENCES "Person" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Event" ("approveLink", "contractAddress", "countOfRewardTokens", "created_at", "creatorID", "description", "finished_at", "id", "isSBT", "locationID", "name", "nftIpfsUrl", "registeredParticipants", "started_at", "statusId", "typeId", "updated_at", "urlCover") SELECT "approveLink", "contractAddress", "countOfRewardTokens", "created_at", "creatorID", "description", "finished_at", "id", "isSBT", "locationID", "name", "nftIpfsUrl", "registeredParticipants", "started_at", "statusId", "typeId", "updated_at", "urlCover" FROM "Event";
+DROP TABLE "Event";
+ALTER TABLE "new_Event" RENAME TO "Event";
+CREATE UNIQUE INDEX "Event_nftIpfsUrl_key" ON "Event"("nftIpfsUrl");
+CREATE UNIQUE INDEX "Event_contractAddress_key" ON "Event"("contractAddress");
+CREATE UNIQUE INDEX "Event_approveLink_key" ON "Event"("approveLink");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
