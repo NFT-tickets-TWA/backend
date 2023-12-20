b0835936357db3b8e3ff338403d90bde30018684
blue <336767@niuitmo.ru>
2023-12-20 23:21:17 +0300

    reg id

--- src/participant-list/dto/create-participant-list.input.ts
+++ src/participant-list/dto/create-participant-list.input.ts
@@ -2,8 +2,8 @@ import {InputType, Int, Field, ID} from '@nestjs/graphql';
 import { ParticipantList as ParticipantListDB } from '@prisma/client';
 @InputType()
 export class CreateParticipantListInput {
-  @Field(() => ID)
-  personID: ParticipantListDB["personID"];
-  @Field(() => ID)
-  eventID: ParticipantListDB["eventID"];
+  @Field(() => Int)
+  personID: number;
+  @Field(() => Int)
+  eventID: number;
 }
